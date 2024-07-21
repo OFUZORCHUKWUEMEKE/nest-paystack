27d54354043f4469c949887b3c53ff7133b9c167
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-07-21 18:56:13 +0100

    swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ExceptionsLoggerFilter } from './common/exceptions/bases';
 import { HttpExceptionFilter } from './common/exceptions/exception';
 import { ModelExceptionFilter } from './common/filters/exceptions/model-exception.filter';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import config from './config/config';
 
 async function bootstrap() {
@@ -15,6 +16,17 @@ async function bootstrap() {
   app.use(cookieParser())
 
   app.useGlobalFilters(new HttpExceptionFilter(), new ModelExceptionFilter());
+
+  const config = new DocumentBuilder()
+  .setTitle('Nestjs Paystack Integration')
+  .setDescription('Paystack Integration with nestjs with a wallet feature')
+  .setVersion('1.0')
+  .addTag('Haulage API')
+  .build();
+
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
